8436e6cf685919e80f093001026e92100a3b37a2
Ahmed <ahmedelmansy579@gmail.com>
2025-08-20 20:48:00 +0300

    fix: fixed

--- src/components/CrmComponents/DealType.tsx
+++ src/components/CrmComponents/DealType.tsx
@@ -43,11 +43,6 @@ export default function DealType() {
                 fontSize: "12px",
                 fontWeight: 600,
                 color: "#495361",
-                // formatter: function (w) {
-                //   return w.globals.seriesTotals.reduce((a, b) => {
-                //     return a + b;
-                //   }, 0);
-                // },
               },
             },
           },
